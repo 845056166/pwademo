1f735fb0bced84897d1de07fac45e4a04999e0d2
845056166@qq.com <845056166@qq.com>
2019-12-29 10:08:00 +0800

    SR_init

---
+++ sw.js
@@ -0,0 +1,48 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-8'
+var cacheList=[
+  '/',
+  'index.html',
+  'main.css',
+  'test.jpg',
+  'mainifest.json'
+]
+self.addEventListener('install',e =>{  // install 事件，它发生在浏览器安装并注册 Service Worker 时
+  // e.waitUtil 用于在安装成功之前执行一些预装逻辑
+  console.log('installed')
+  console.log('caches', caches);
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+self.addEventListener('fetch',function(e){
+  console.log('sth fetched');
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+      if(response != null){
+        return response
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
+self.addEventListener('activated',function(e){
+  console.log('activated');
+  e.waitUntil(
+    //获取所有cache名称
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        // 获取所有不同于当前版本名称cache下的内容
+        cacheNames.filter(cacheNames => {
+          return cacheNames !== cacheStorageKey
+        }).map(cacheNames => {
+          return caches.delete(cacheNames)
+        })
+      )
+    }).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
